bdafc55e207ddec47611fdf5fcd2b4f6f36b19a2
pglpm <luca@magnaspesmeretrix.org>
2024-03-20 13:29:39 +0100

    update

---
+++ rb2d_uprightyank.m
@@ -0,0 +1,13 @@
+ma = 1; % mass of object a
+mb = 1; % mass of object b
+xa0 = 5; ya0 = 0; % initial position of object a
+xb0 = 0; yb0 = 0; % initial position of object a
+vxa0 = -10; vya0 = 10; % initial velocity of object a
+vxb0 = 0; vyb0 = 0; % initial velocity of object b
+Gxa=0; Gya=0; # gravity supply on object a
+Gxb=0; Gyb=0; # gravity supply on object b
+k = 500; % spring constant
+ln = 5; % natural length of rubber band
+t0 = 0; % initial time
+t1 = 10; % final time
+dt = 0.0001; % time step
