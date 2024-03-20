091c4e5521f3500c5b8161f24bd25c064149434b
pglpm <luca@magnaspesmeretrix.org>
2024-03-20 23:10:50 +0100

    update

--- rb2d_forcedfall.m
+++
@@ -1,9 +0,0 @@
-ma = 1; % mass of object a
-mb = 10000; % mass of object b
-xa0 = 3; ya0 = 0; % initial position of object a
-xb0 = 0; yb0 = 0; % initial position of object a
-vxa0 = 0; vya0 = -10; % initial velocity of object a
-vxb0 = 0; vyb0 = 0; % initial velocity of object b
-Gxa=0; Gya=-ma*9.8; # gravity supply on object a
-Gxb=0; Gyb=0; # gravity supply on object b
-k = 5; % spring constant
