961f518e5818e54d22bb6e59c1e636a1c92b88a7
pglpm <luca@magnaspesmeretrix.org>
2024-04-02 22:42:07 +0200

    update

--- code/fluid.m
+++ code/fluid.m
@@ -1,18 +1,24 @@
 %% SI units used throughout
 %% Simulation of ideal gas in 1D
 %% Coordinate z
-N = 1; % amount of matter
-L0 = 1; % initial volume
-T0 = 273.15+25; % initial temp
-Q = 0; % heat flux
+N = 0.01; % amount of ideal gas
+A = 0.01; % area of piston
+z0 = 0.51; % initial position of mass above piston
+v0 = 0; % initial velocity of mass above piston
+T0 = 273.15+25; % initial temperature of gas
+Q = 0; % heat influx
+m = 5; % mass above piston
+P0 = 1e5; % atmospheric pressure
 %%
 R = 8.31446261815; % gas constant
 C = 3*R/2; % molar heat capacity
-H = 18e-6; % shear viscosity
+H = 18e1 *4/3; % shear viscosity
+g = 9.80665; % gravitational acceleration
+G = -m*g; % supply of momentum to mass above piston
 %%
 t0 = 0; % initial time
 t1 = 10; % final time, can also be earlier than initial
-dt = 0.001; % time step, negative if backward time integration %@
+dt = 0.0001; % time step, negative if backward time integration %@
 %% adjust final time if not multiple of timestep
 t1 = t1 + mod(t1-t0,dt);
 %% check if timestep and time interval are consistent
@@ -29,76 +35,62 @@ if abs(dsave) < abs(dt)
   error('time interval between saves is smaller than timestep')
 end
 %% Initialize vectors to contain saved values
-tSave = nan(Nsaves,1); % time
-yaSave = nan(Nsaves,1); zaSave = nan(Nsaves,1);
-ybSave = nan(Nsaves,1); zbSave = nan(Nsaves,1);
-PyaSave = nan(Nsaves,1); PzaSave = nan(Nsaves,1);
-PybSave = nan(Nsaves,1); PzbSave = nan(Nsaves,1);
+tSave = nan(Nsaves,1);
+zSave = nan(Nsaves,1);
+vSave = nan(Nsaves,1);
+TSave = nan(Nsaves,1);
+FSave = nan(Nsaves,1);
 %% Save initial values
 i = 1; % index that keeps count of savepoints
 tSave(1) = t0;
-yaSave(1) = ya0; zaSave(1) = za0;
-ybSave(1) = yb0; zbSave(1) = zb0;
-PyaSave(1) = ma*vya0; PzaSave(1) = ma*vza0;
-PybSave(1) = mb*vyb0; PzbSave(1) = mb*vzb0;
+zSave(1) = z0;
+vSave(1) = v0;
+TSave(1) = T0;
+FSave(1) = N*R*T0/z0 - A*H*v0/z0;
 %% Initialize plot
 cols = get(0, 'DefaultAxesColorOrder');
-plot(yaSave(1), zaSave(1), 's','Color',cols(1,:)); axis('tight');
-plot(ybSave(1), zbSave(1), 'o','Color',cols(1,:)); axis('tight');
-xlabel('{\it y}/m'); ylabel('{\it z}/m'); hold on;
+plot(tSave(1), zSave(1), 'o','Color',cols(1,:)); axis('tight');
+xlabel('{\it t}/s'); ylabel('{\it z}/m'); hold on;
 %% %@
 %% Numerical time integration
 %% Initialize
 t = t0;
-ya = ya0; za = za0;
-yb = yb0; zb = zb0;
-vya = vya0; vza = vza0;
-vyb = vyb0; vzb = vzb0;
-Pya = ma*vya0; Pza = ma*vza0;
-Pyb = mb*vyb0; Pzb = mb*vzb0;
+z = z0;
+v = v0;
+T = T0;
+U = C*N*T0;
+P = m*v0;
 %% loop
 while sign(dt)*t < sign(dt)*t1 % possible backward time integration
   %% update time
   t = t + dt;
-  %% non-Hookean constitutive relation
-  l = norm([ya-yb, za-zb]); % present length
-  if l < ln
-    Fyab = 0;
-    Fzab = 0;
-  else
-    Fyab = -k*(ya-yb)*(l-ln)/l;
-    Fzab = -k*(za-zb)*(l-ln)/l;
-  end
-  %% update momentum
-  Fyba = -Fyab;
-  Fzba = -Fzab;
-  Pya = Pya + (Fyab + Gya)*dt;
-  Pza = Pza + (Fzab + Gza)*dt;
-  Pyb = Pyb + (Fyba + Gyb)*dt;
-  Pzb = Pzb + (Fzba + Gzb)*dt;
-  %% update velocity
-  vya = Pya/ma;
-  vza = Pza/ma;
-  vyb = Pyb/mb;
-  vzb = Pzb/mb;
-  %% update position
-  ya = ya + vya*dt;
-  za = za + vza*dt;
-  yb = yb + vyb*dt;
-  zb = zb + vzb*dt; %@
+  %% constitutive relation for force on mass above piston
+  %% same as *minus* force on gas
+  F = N*R*T/z - A*H*v/z;
+  %% update internal energy of gas
+  U = U + (Q - F*v)*dt;
+  %% update temperature of gas
+  T = U/(C*N);
+  %% update momentum of mass above piston
+  P = P + (F + G)*dt;
+  %% update velocity of mass above piston
+  v = P/m;
+  %% update position of mass above piston
+  z = z + v*dt; %@
   %% Check whether to save & plot at this step
   if min(abs([0 dsave] - mod(t-t0, dsave))) <= abs(dt)/2
     i = i+1;
     tSave(i) = t;
-    yaSave(i) = ya; zaSave(i) = za;
-    ybSave(i) = yb; zbSave(i) = zb;
-    PyaSave(i) = Pya; PzaSave(i) = Pza;
-    PybSave(i) = Pyb; PzbSave(i) = Pzb;
-    plot(ya, za, 's','Color',cols(1,:));
-    plot(yb, zb, 'o','Color',cols(2,:));
+    zSave(i) = z;
+    vSave(i) = v;
+    TSave(i) = T;
+    FSave(i) = F;
+    plot(t, z, 'o','Color',cols(1,:));
     pause(0.001);
   end %@
 end %@
 %% Plot trajectory
-plot(yaSave,zaSave,'-','Color',cols(1,:));
-plot(ybSave,zbSave,'-.','Color',cols(2,:)); %@
+plot(tSave,zSave,'-','Color',cols(1,:));
+figure();
+plot(tSave,TSave-273.15,'-','Color',cols(2,:)); axis('tight');
+xlabel('{\it t}/s'); ylabel('{\it T}/C');
