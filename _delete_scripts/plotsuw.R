2346f0d130d73ab9cb4c061998b5ed9c2889d213
pglpm <pgl@portamana.org>
2025-03-26 07:40:50 +0100

    update chapter 12

--- _delete_scripts/plotsuw.R
+++
@@ -1,3 +0,0 @@
-pdff('uplot',portrait=F);tplot(x=1:256,y=1:256,xlabels=NA,ylabels=NA,xlim=c(NA,NA),ylim=c(-50,NA),xlab=bquote(italic(t)),ylab=bquote(italic(u)[italic(x)]),lwd=4,cex.lab=4,mar=c(NA,4,NA,1),ly=1,family='Palatino');dev.off()
-
-pdff('wplot',portrait=F);tplot(x=1:256,y=sin(2*pi*(1:256)/75+5),xlabels=NA,ylabels=NA,xlab=bquote(italic(t)),ylab=bquote(italic(w)[italic(x)]),lwd=4,cex.lab=4,ly=1,mar=c(NA,4,NA,1),family='Palatino');abline(h=0,lwd=3);dev.off()
