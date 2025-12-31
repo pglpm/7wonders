6077f0712cab8abb8bd807e554a0314ae1278838
pglpm <pgl@portamana.org>
2025-12-31 09:29:34 +0100

    update rocket

--- scripts/rocket_simplified.m
+++ scripts/rocket_simplified.m
@@ -2,35 +2,48 @@
 %% Coordinates (t, z)
 
 %% Constants
-m = 756e3;         % kg: rocket mass without fuel
-rho = 0.17;        % kg/mol: fuel molar mass
-delta = 25;            % mol/m^3: fuel molar density
-A = 7.5;           % m^2: nozzle area
-h = 0.3;           % N/(m^2/s^2): drag coefficient
-ka = 6.67430e-11;  % N/(kg^2/m^2): gravity const.
-g = 9.8;      % kg: Earth's mass
+%2900000 kg total
+%2145798 kg fuel stage I
+%1499479 kg LOX
+%646318 kg RP-1
+%792516 kg total without fuel I
+%0.19822 g/molRP-1 molar mass
+%0.219 kg/mol LOX/RP-1
+%2.6e3 kg/s mass flow * 5 -> 1.1e4 mol/s * 5 = 6e5 mol/s
+%9753627 mol fuel
+%6.8e6 N * 5 thrust sea level
+%34e6 N (orloff)
+%4000000 N/m^2 pressure
+% 3200 or 1600 K temp ->
+% 150 or 300 mol/m^3
+m = 7.9e5;         % kg: rocket mass without fuel
+rho = 0.22;        % kg/mol: fuel molar mass
+delta = 1.5e2;            % mol/m^3: fuel molar density
+A = 50;           % m^2: nozzle area
+g = 9.8;      % kg: free-fall acceleration
 
 %% Initial conditions
 t = 0;       % s: initial time
-N = 12.2e6;  % mol: amount of fuel
+N = 9.8e6;  % mol: amount of fuel
 z = 0;       % m: altitude of control volume
 v = 0;       % m/s: velocity of control volume
 
 %% Boundary conditions
-J = -80e3;  % mol/s: matter influx at nozzle
-p = 3.7e6;  % N/m^2: pressure at nozzle
+J = -7.3e4;  % mol/s: matter influx at nozzle
+p = 4e6;  % N/m^2: pressure at nozzle
+Sigma = A * p;
 
 %% Time-iteration parameters
-t1 = 160;    % s: final time
-dt = 0.001;  % s: time step
+t1 = 130;    % s: final time
+dt = 0.0001;  % s: time step
 
 %% Plotting
 dtplot = t1/360; % time interval between plots
 tplot = dtplot; % time for next plot
 figure
-subplot(2, 1, 1); plot(t, 0, '.b')
+subplot(2, 1, 1); plot(t, z, '.b')
 xlim([0, t1])
-xlabel('time {\it t}/s'); ylabel('{\it Fconv}/N')
+xlabel('time {\it t}/s'); ylabel('{\it z}/m')
 axis('tight'); grid on; hold on
 subplot(2, 1, 2); plot(t, v, '.r')
 xlim([0, t1])
@@ -38,12 +51,12 @@ xlabel('time {\it t}/s'); ylabel('{\it v} / (m/s)')
 axis('tight'); grid on; hold on %@
 
 %% Numerical time integration
-while t < t1 && v < 300 && N > 0 % '&&' means 'and'
+while t < t1 && N > 0 % '&&' means 'and'
 
   P = (m + rho * N) * v;
   vb = J / (A * delta) + v;
-  F = Sigma + J * delta * rho * vb;
-  G = (m + rho * N) * g;
+  F = Sigma + J * rho * vb;
+  G = -(m + rho * N) * g;
 
   t = t + dt;
   N = N + J * dt;
@@ -54,7 +67,7 @@ while t < t1 && v < 300 && N > 0 % '&&' means 'and'
 
   %% plot
   if t > tplot
-    subplot(2, 1, 1); plot(t, Fnoz - A * p, '.b')
+    subplot(2, 1, 1); plot(t, z, '.b')
     subplot(2, 1, 2); plot(t, v, '.r')
     pause(0)
     tplot = tplot + dtplot;
