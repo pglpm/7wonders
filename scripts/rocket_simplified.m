4f177c5c441eaa576b529937271266af5579205c
pglpm <pgl@portamana.org>
2026-01-01 12:18:56 +0100

    update rocket

--- scripts/rocket_simplified.m
+++ scripts/rocket_simplified.m
@@ -2,48 +2,32 @@
 %% Coordinates (t, z)
 
 %% Constants
-%2900000 kg total
-%2145798 kg fuel stage I
-%1499479 kg LOX
-%646318 kg RP-1
-%792516 kg total without fuel I
-%0.19822 g/molRP-1 molar mass
-%0.219 kg/mol LOX/RP-1
-%2.6e3 kg/s mass flow * 5 = 1.3e4 kg/s -> 1.1e4 mol/s * 5 = 6e5 mol/s
-%9753627 mol fuel
-%6.8e6 N * 5 thrust sea level = 3.4e7 N thrust sea level
-% thrust - press = 
-%4000000 N/m^2 pressure
-%vb = 2.5 km/s at start?
-% 3200 or 1600 K temp ->
-% 150 or 300 mol/m^3
-% 0.3 drag coef
-%113 m^2 drag area
-% max vel should be 1620 m/s
-m = 7.9e5;         % kg: rocket mass without fuel
-rho = 0.22;        % kg/mol: fuel molar mass
-delta = 0.46;            % mol/m^3: fuel molar density
-A = 50;           % m^2: nozzle area
+m = 7.9e5;    % kg: rocket mass without fuel
+rho = 0.22;   % kg/mol: fuel molar mass
+delta = 0.5;  % mol/m^3: fuel molar density
+A = 50;       % m^2: nozzle area
 g = 9.8;      % kg: free-fall acceleration
 
 %% Initial conditions
-t = 0;       % s: initial time
+t = 0;      % s: initial time
 N = 9.8e6;  % mol: amount of fuel
-z = 0;       % m: altitude of control volume
-v = 0;       % m/s: velocity of control volume
+z = 0;      % m: altitude of control volume
+v = 0;      % m/s: velocity of control volume
 
 %% Boundary conditions
-J = -6e4;  % mol/s: matter influx at nozzle
-p = 4e4 - 1e5;  % N/m^2: pressure at nozzle
-Sigma = A * p;
+J = -6e4;           % mol/s: matter influx at nozzle
+patm = 1e5;         % N/m^2: atmospheric pressure
+p = 5e4;            % N/m^2: pressure at nozzle
+Fatm = -A * patm;  % N: force on rocket surface
+Sigma = A * p;      % N: stress tensor at nozzle
 
 %% Time-iteration parameters
 t1 = 150;    % s: final time
-dt = 0.001;  % s: time step
+dt = 0.0001;  % s: time step %@
 
 %% Plotting
-dtplot = t1/360; % time interval between plots
-tplot = dtplot; % time for next plot
+dtplot = t1/360;  % time interval between plots
+tplot = dtplot;   % time for next plot
 figure
 subplot(2, 1, 1); plot(t, z, '.b')
 xlim([0, t1])
@@ -59,7 +43,7 @@ while t < t1 && N > 0 % '&&' means 'and'
 
   P = (m + rho * N) * v;
   vb = v + J / (A * delta);
-  F = Sigma + J * rho * vb; % - 113 * 0.35 * abs(v) * v / 2;
+  F = Fatm + Sigma + J * rho * vb;
   G = -(m + rho * N) * g;
 
   t = t + dt;
@@ -67,7 +51,7 @@ while t < t1 && N > 0 % '&&' means 'and'
   P = P + (F + G) * dt;
   z = z + v * dt;
 
-  v = P / (m + rho * N);
+  v = P / (m + rho * N); %@
 
   %% plot
   if t > tplot
@@ -76,5 +60,4 @@ while t < t1 && N > 0 % '&&' means 'and'
     pause(0)
     tplot = tplot + dtplot;
   end %@
-
 end
