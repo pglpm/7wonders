8f97a5ce9017c21aa9e6ee72456de0780540e8e4
pglpm <pgl@portamana.org>
2026-01-21 19:48:19 +0100

    update matter

--- scripts/radioactive_decay.m
+++ scripts/radioactive_decay.m
@@ -4,15 +4,15 @@
 lambda = 0.000122;  % 1/yr: decay constant for C14
 
 %% Initial conditions
-t = 0;	   % s: initial time
-N = 1e-12  % mol: initial amount of C14
+t = 0;      % s: initial time
+N = 1e-12;  % mol: initial amount of C14
 
 %% Boundary conditions
 J = 0;  % mol/s: matter influx of C14
 
 %% Parameters for time loop
 t1 = 20000;  % yr: final time
-dt = 0.1;    % yr: time step %@
+dt = 1;    % yr: time step %@
 
 %% Plotting
 dtplot = t1/360;  % time interval between plots
@@ -27,7 +27,7 @@ while t < t1
   %% constitutive relations
   R = -lambda * N;
 
-  %% balances
+  %% step forward in time with balance law
   t = t + dt;
   N = N + (J + R) * dt; %@
 
