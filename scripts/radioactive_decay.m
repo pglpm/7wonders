7709bc199231a6e34a71cb81b75641062ca0524f
pglpm <pgl@portamana.org>
2026-01-21 08:13:03 +0100

    update matter

---
+++ scripts/radioactive_decay.m
@@ -0,0 +1,40 @@
+%%% Time integration of radioactive decay
+
+%% Constants
+lambda = 0.000122;  % 1/yr: decay constant for C14
+
+%% Initial conditions
+t = 0;	   % s: initial time
+N = 1e-12  % mol: initial amount of C14
+
+%% Boundary conditions
+J = 0;  % mol/s: matter influx of C14
+
+%% Parameters for time loop
+t1 = 20000;  % yr: final time
+dt = 0.1;    % yr: time step %@
+
+%% Plotting
+dtplot = t1/360;  % time interval between plots
+tplot = dtplot;	  % time for next plot
+figure
+plot(t, N, '.', 'color', '#4477AA')
+xlabel('{\it t}/s'); ylabel('{\it N}/mol')
+axis('tight'); grid on; hold on %@
+
+%% Numerical time integration
+while t < t1
+  %% constitutive relations
+  R = -lambda * N;
+
+  %% balances
+  t = t + dt;
+  N = N + (J + R) * dt; %@
+
+  %% plot
+  if t > tplot
+    plot(t, N, '.', 'color', '#4477AA')
+    pause(0)
+    tplot = tplot + dtplot;
+  end %@
+end
