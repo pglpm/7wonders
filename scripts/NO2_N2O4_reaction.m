db328fdbac2887c9994ce5fe9957a212d89b8cec
pglpm <pgl@portamana.org>
2026-01-23 22:57:21 +0100

    update matter

--- scripts/NO2_N2O4_reaction.m
+++ scripts/NO2_N2O4_reaction.m
@@ -3,30 +3,30 @@
 
 %% Constants
 R = 8.31446261815324;  % J/(K*mol): molar gas constant
-A1 = 1.88e5;         % m^3/(mol*s): pre-exp. NO2
-E1 = -2.23e3;        % J/mol: activation en. NO2
-A2 = 4.30e15;       % 1/s: pre-exp N2O4
-E2 = 5.32e4;        % J/mol: activation en. N2O4
+A1 = 1.88e5;           % m^3/(mol*s): pre-exp. NO2
+E1 = -2.23e3;          % J/mol: activation en. NO2
+A2 = 4.30e15;          % 1/s: pre-exp N2O4
+E2 = 5.32e4;           % J/mol: activation en. N2O4
 
 %% Initial conditions. State: (N_NO2, N_N2O4, z, v, T)
-t = 0;            % s: time
-N_NO2 = 0.00;      % mol: amount NO2
-N_N2O4 = 0.04;     % mol: amount N2O4
+t = 0;          % s: time
+N_NO2 = 0.00;   % mol: amount NO2
+N_N2O4 = 0.04;  % mol: amount N2O4
 
 %% Boundary conditions
-T = 340;  % K: temperature
-V = 0.001;            % m^3: volume
+T = 340;    % K: temperature
+V = 0.001;  % m^3: volume
 
 %% Time-iteration parameters
-t1 = 2e-7;        % s: final time
-dtmin = 1e-10;  % s: time step %@
+t1 = 2e-7;   % s: final time
+dt = 2e-10;  % s: time step %@
 
 %% Plotting
 dtplot = t1/360;  % time interval between plots
 tplot = dtplot;   % time for next plot
-clf
+figure
 plot(t, N_NO2, 'o', 'color', '#EE6677')
-hold on ;
+hold on;
 plot(t, N_N2O4, 'o', 'color', '#CCBB44')
 legend('NO_2', 'N_2O_4')
 legend('box', 'off', 'location', 'southeast', 'fontsize', 24)
@@ -44,7 +44,6 @@ while t < t1
   R_N2O4 = R_NO2/(-2);
 
   %% step forward in time with balance laws
-  %% adaptive timestep, temporary
   t = t + dt;
   N_NO2 = N_NO2 + R_NO2 * dt;
   N_N2O4 = N_N2O4 + R_N2O4 * dt; %@
