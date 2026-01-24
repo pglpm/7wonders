237603009ec6d2aa8f6a6186fa350c5d3908bb39
pglpm <pgl@portamana.org>
2026-01-24 14:09:47 +0100

    update matter and layout

---
+++ docs/scripts/radioactive_decay.py
@@ -0,0 +1,45 @@
+from matplotlib.pyplot import *
+from numpy import *
+from numpy.linalg import norm
+from numpy import array as vector
+import matplotlib.pyplot as plt
+### Time integration of radioactive decay
+
+## Constants
+lambd = 0.000122  # 1/yr: decay constant for C14
+
+## Initial conditions
+t = 0	   # s: initial time
+N = 1e-12  # mol: initial amount of C14
+
+## Boundary conditions
+J = 0  # mol/s: matter influx of C14
+
+## Parameters for time loop
+t1 = 20000  # yr: final time
+dt = 0.1    # yr: time step #@
+
+## Plotting
+dtplot = t1/360  # time interval between plots
+tplot = dtplot	  # time for next plot
+figure
+plot(t, N, 'o', color = '#4477AA')
+xlabel('${\it t}$/s'); ylabel('${\it N}$/mol')
+grid(True);  #@
+
+## Numerical time integration
+while t < t1:
+  ## constitutive relations
+  R = -lambd * N
+
+  ## step forward in time with balance law
+  t = t + dt
+  N = N + (J + R) * dt #@
+
+  ## plot
+  if t > tplot:
+    plot(t, N, 'o', color = '#4477AA')
+    # pause(0.0001) # comment for matplotlib.online
+    tplot = tplot + dtplot
+
+plt.show()
---
+++ docs/scripts/rocket_simplified_air.m
@@ -0,0 +1,69 @@
+%%% Numerical simulation of rocket propulsion
+%% Coordinates (t, z)
+
+%% Constants
+m = 7.9e5;    % kg: rocket mass without fuel
+rho = 0.22;   % kg/mol: fuel molar mass
+delta = 0.5;  % mol/m^3: fuel molar density
+A = 50;       % m^2: nozzle area
+g = 9.8;      % kg: free-fall acceleration
+C = 13;       % N/(m/s)^2: total drag coeff.
+
+
+%% Initial conditions. State: (z, v, Nb)
+t = 0;       % s: initial time
+z = 0;       % m: altitude of control volume
+v = 0;       % m/s: velocity of control volume
+Nb = 9.8e6;  % mol: amount of fuel
+
+%% Boundary conditions
+J = -6e4;          % mol/s: matter influx at nozzle
+patm = 1e5;        % N/m^2: atmospheric pressure
+p = 5e4;           % N/m^2: pressure at nozzle
+Fatm = -A * patm;  % N: force on rocket surface
+Sigma = A * p;     % N: stress tensor at nozzle
+
+%% Time-iteration parameters
+t1 = 1;     % s: final time
+dt = 0.0001;  % s: time step %@
+
+%% Plotting
+dtplot = t1/360;  % time interval between plots
+tplot = dtplot;   % time for next plot
+figure
+subplot(2, 1, 1); plot(t, z, '.', 'color', '#4477AA')
+xlim([0, t1])
+xlabel('time {\it t}/s'); ylabel('{\it z}/m')
+axis('tight'); grid on; hold on
+subplot(2, 1, 2); plot(t, v, '.', 'color', '#EE6677')
+xlim([0, t1])
+xlabel('time {\it t}/s'); ylabel('{\it v} / (m/s)')
+axis('tight'); grid on; hold on %@
+
+%% Numerical time integration
+while t < t1 && Nb > 0 % '&&' means 'and'
+  %% constitutive relations
+  P = (m + rho * Nb) * v;
+  vb = v + J / (A * delta); % needed to calculate F
+  Fair = Fatm - C * abs(v) * v;
+  Fnoz = Sigma + J * rho * vb;
+  F = Fair + Fnoz;
+  G = -(m + rho * Nb) * g;
+
+  %% step forward in time with balance laws
+  t = t + dt;
+  Nb = Nb + J * dt;
+  P = P + (F + G) * dt;
+  z = z + v * dt;
+
+  %% constitutive relations: calculate state
+  v = P / (m + rho * Nb); %@
+
+  %% plot
+  if t > tplot
+    subplot(2, 1, 1); plot(t, z, '.', 'color', '#4477AA')
+    subplot(2, 1, 2); plot(t, v, '.', 'color', '#EE6677')
+    pause(0)
+    tplot = tplot + dtplot;
+  end %@
+end
