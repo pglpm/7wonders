fc166928d7f083d5f6357514c9d0edb367836585
pglpm <pgl@portamana.org>
2026-03-06 11:42:02 +0100

    update scripts

--- scripts/billiard_impact.m
+++ scripts/billiard_impact.m
@@ -12,10 +12,10 @@ rcoeff = -Ic / (m * R^2 + Ic); % rolling-friction coeff.
 
 %% Initial conditions, state: (r1, v1, o1, r2, v2, o2)
 t = 0;
-r1 = [0, 0];
+r1 = [0.02, -0.05];
 v1 = [0, 1];
 o1 = [-v1(2), v1(1), -10] / R;
-r2 = [-2*R * 3/4, 0.2];
+r2 = [-0.02, 0.05];
 v2 = [0, 0];
 o2 = [-v2(2), v2(1), 0] / R;
 
@@ -26,28 +26,34 @@ Ltot = cross([r1, 0], [m * v1, 0]) + Ic * o1 + ...
 Ek = 0.5 * m * (norm(v1)^2 + norm(v2)^2) + ...
      0.5 * Ic * (norm(o1)^2 + norm(o2)^2)
 
+Ptotini = Ptot;
+Ltotini = Ltot;
+Ekini = Ek;
+
 %% Boundary conditions
 
 %% Parameters for time loop
-t1 = 0.3; % s: final time
-dt = 0.0001; % s: time step %@
+t1 = 0.15; % s: final time
+dt = 0.00001; % s: time step %@
 
 %% Plotting
 dtplot = t1/360;  % time interval between plots
 tplot = dtplot;   % time for next plot
 clf
+C = 0.2;
+ms = R * 230 / C;
 subplot(2, 1, 1)
-plot(r1(1), r1(2), 'o', 'color', '#4477AA', 'markersize', 4); hold on
-plot(r2(1), r2(2), 'o', 'color', '#EE6677', 'markersize', 4)
+plot(r1(1), r1(2), 'o', 'color', '#4477AA', 'markersize', ms); hold on
+plot(r2(1), r2(2), 'o', 'color', '#EE6677', 'markersize', ms)
 xlabel('{\it x}/m'); ylabel('{\it y}/m')
-xlim([-1, 1]); ylim([-1, 1])
+xlim([-C, C]); ylim([-C, C])
 axis('equal'); grid on;
 subplot(2, 1, 2)
 plot(t, Ek, 'ok'); hold on
 xlabel('{\it t}/s'); ylabel('{\it E_k}/J')
 xlim([0, t1]); axis('tight'); grid on;
 pause(0) %@
-
+return
 %% Numerical time integration
 while t < t1
   %% constitutive relations
@@ -118,10 +124,10 @@ while t < t1
   %% plot
   if t > tplot
     subplot(2, 1, 1); hold off
-    plot(r1(1), r1(2), 'o', 'color', '#4477AA', 'markersize', 4); hold on
-    plot(r2(1), r2(2), 'o', 'color', '#EE6677', 'markersize', 4)
+    plot(r1(1), r1(2), 'o', 'color', '#4477AA', 'markersize', ms); hold on
+    plot(r2(1), r2(2), 'o', 'color', '#EE6677', 'markersize', ms)
     xlabel('{\it x}/m'); ylabel('{\it y}/m')
-    xlim([-1, 1]); ylim([-1, 1])
+    xlim([-C, C]); ylim([-C, C])
     axis('equal'); grid on;
     subplot(2, 1, 2)
     plot(t, Ek, 'ok');
@@ -135,3 +141,8 @@ Ltot = cross([r1, 0], [m * v1, 0]) + Ic * o1 + ...
        cross([r2, 0], [m * v2, 0]) + Ic * o2
 Ek = 0.5 * m * (norm(v1)^2 + norm(v2)^2) + ...
      0.5 * Ic * (norm(o1)^2 + norm(o2)^2)
+
+
+Ptot - Ptotini
+Ltot - Ltotini
+Ek - Ekini
